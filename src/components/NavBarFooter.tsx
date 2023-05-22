ba0e097b53c5d55e1c6a52072ad933aa5dae1067
Edgard Araujo <edgard.araujo@al.infnet.edu.br>
2023-05-22 20:36:53 -0300

    Template Experiencia profissional e jpegs das empresas

--- src/components/NavBarFooter.tsx
+++ src/components/NavBarFooter.tsx
@@ -3,30 +3,33 @@ import { Link } from 'react-router-dom';
 export function NavBarFooter() {
   return (
     <footer className='bg-sky-800 fixed bottom-0 p-2 w-full text-lg font-semibold text-center flex flex-row justify-center gap-16'>
-      <Link to='/' className='text-gray-50 cursor-pointer hover:text-cyan-200'>
+      <Link
+        to='/'
+        className='text-gray-50 cursor-pointer hover:text-cyan-200 hover:underline'
+      >
         Início
       </Link>
       <Link
         to='/ProExperience'
-        className='text-gray-50 cursor-pointer hover:text-cyan-200'
+        className='text-gray-50 cursor-pointer hover:text-cyan-200 hover:underline'
       >
         Experiências
       </Link>
       <Link
         to='/AcademicEducation'
-        className='text-gray-50 cursor-pointer hover:text-cyan-200'
+        className='text-gray-50 cursor-pointer hover:text-cyan-200 hover:underline'
       >
         Formação
       </Link>
       <Link
         to='/Contact'
-        className='text-gray-50 cursor-pointer hover:text-cyan-200'
+        className='text-gray-50 cursor-pointer hover:text-cyan-200 hover:underline'
       >
         Contato
       </Link>
       <Link
         to='/Notepad'
-        className='text-gray-50 cursor-pointer hover:text-cyan-200'
+        className='text-gray-50 cursor-pointer hover:text-cyan-200 hover:underline'
       >
         Notepad
       </Link>
--- src/routes/ProExperienceRoute.tsx
+++ src/routes/ProExperienceRoute.tsx
@@ -1,3 +1,42 @@
 export function ProExperienceRoute() {
-  return <div>Experiência Profissional</div>;
+  return (
+    <div className='lg:w-[48rem] m-auto pt-7'>
+      <div className='text-gray-800'>
+        <h3 className='text-2xl pb-2'>Experiência Profissional</h3>
+        <div className='flex flex-row items-center gap-3'>
+          <div className=''>
+            <a
+              href='https://www.ey.com/pt_br'
+              target='_blank'
+              className='cursor-pointer hover:text-cyan-600 hover:underline'
+            >
+              <img
+                className='rounded-full border-solid border-2 border-sky-800 hover:border-sky-400'
+                src='/src/assets/img/ey.jpeg'
+                alt='Logo EY'
+              ></img>
+            </a>
+          </div>
+          <div>
+            <p>
+              <a
+                href='https://www.ey.com/pt_br'
+                target='_blank'
+                className='cursor-pointer hover:text-cyan-600 hover:underline'
+              >
+                EY
+              </a>
+            </p>
+            <p>Consultor sênior</p>
+            <p>Modalidade híbrida</p>
+            <p>Janeiro de 2023 - até o presente momento</p>
+            <p>Rio de Janeiro, Rio de Janeiro, Brasil</p>
+          </div>
+        </div>
+        <div>
+          <p>Em construção</p>
+        </div>
+      </div>
+    </div>
+  );
 }
