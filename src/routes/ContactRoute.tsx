86dfb94266ff70581fbad466e0b6affcdeac6c7f
Edgard Araujo <edgard.araujo@al.infnet.edu.br>
2023-05-22 21:13:50 -0300

    Inicio contato

--- src/routes/ContactRoute.tsx
+++ src/routes/ContactRoute.tsx
@@ -1,3 +1,10 @@
 export function ContactRoute() {
-  return <div>Contato</div>;
+  return (
+    <div className='lg:w-[48rem] m-auto pt-7'>
+      <div className='text-gray-800'>
+        <h3 className='text-2xl pb-2'>Contato</h3>
+        <div className=''></div>
+      </div>
+    </div>
+  );
 }
